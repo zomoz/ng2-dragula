0b45eca5429edf15e6b599f15852a9b536a9ea40
Juan Carlos Arocha <arochajuan2@gmail.com>
2017-11-11 19:52:49 -0400

    use custom npm packages

--- demo/src/app/app.module.ts
+++ demo/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { CommonModule } from '@angular/common';
-import { DragulaModule } from 'ng2-dragula';
+import { DragulaModule } from '@zomoz/ng2-dragula';
 
 import { EXAMPLES } from './examples';
 import { DemoComponent } from './app.component';
--- src/components/dragula.class.ts
+++ src/components/dragula.class.ts
@@ -1,2 +1,4 @@
-import * as dragulaExpt from 'dragula';
+declare const require: any;
+
+const dragulaExpt = require('@zomoz/dragula');
 export const dragula: (containers?: any, options?: any) => any = (dragulaExpt as any).default || dragulaExpt;
